2513cad71c3a60b1b1d721660f4691d03eff67c8
vinithaneyashree <vinithaneyashree2901@gmail.com>
2022-07-01 18:42:00 +0530

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import {Routes,Route,} from "react-router-dom";
+
 import './App.css';
 
+import Forgotpassword from "./Components/Forgotpassword";
+import Msg from "./Components/Msg";
+import Resetpassword from "./Components/Resetpassword";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/" element={<Forgotpassword />} />
+      <Route path="/resetpassword/:pass_token" element={<Resetpassword />} />
+      <Route path="/msg" element={<Msg />} />
+      </Routes>
+    
+
+    <ToastContainer autoclose={7000} />
     </div>
+    
   );
 }
 
